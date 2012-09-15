df63e4db74bf6b9da32b4357146e3db575320f6d
3rawrpandas <3rawrpandas@gmail.com>
2012-09-15 02:34:49 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IAmBlank::Application.config.secret_token = '4f50dc6ba190f5f0b8e3a8ec0ced090c17ee7cf85e4fc83d964846d8add5f7404fe6fbffa1340c6e1fe996a1b60822e8c87ddcfce5c24a0d1672961d9e588d5c'
